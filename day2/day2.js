6fbea8f37cd941f7bd55e191a3a62733a4b79bd4
Myeongjae <tjfaudwo0818@gmail.com>
2021-11-30 21:08:06 +0900

    day2 homework

---
+++ day2/day2.js
@@ -0,0 +1,18 @@
+// 문제 1
+function gugudan(n) {
+  // 숫자 n을 입력받아서 구구단 n단을 출력하는 함수 작성
+}
+
+gugudan(Math.floor(Math.random() * 10));
+
+// 문제 2
+function star() {
+  // console.log로 아래 모양처럼 * 찍기 힌트 - for문 두번 사용
+  //*
+  //**
+  //***
+  //****
+  //*****
+}
+
+star();
