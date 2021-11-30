e57dc4986cf56fb70e8af35e6bdfb2bfa59e06f5
2hun-ai <lllii1111@naver.com>
2021-11-30 23:54:55 +0900

    ㅠㅠ

---
+++ day2/jh2.js
@@ -0,0 +1,21 @@
+// 문제 1
+// 숫자 n을 입력받아서 구구단 n단을 출력하는 함수 작성
+// function gugudan(n) {
+//   for (i = 1; i <= 9; i++) console.log(n * i);
+// }
+
+// gugudan(Math.floor(Math.random() * 10));
+
+// 문제 2
+function star() {
+    let x = ['*']
+    for (i=0; i <)
+  // console.log로 아래 모양처럼 * 찍기 힌트 - for문 두번 사용
+  //*
+  //**
+  //***
+  //****
+  //*****
+}
+
+star();
